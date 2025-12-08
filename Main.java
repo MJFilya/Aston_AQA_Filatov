bb014b1797cf1a3bc1ce13d23f50bb1b5e864b2c
MJFilya <131555225+MJFilya@users.noreply.github.com>
2025-12-08 21:04:23 +0300

    Add files via upload
    Лекции 2.2.

---
+++ Main.java
@@ -0,0 +1,182 @@
+public class Main {
+    public static void main(String[] args) {
+        printThreeWords();
+//        checkSumSign();
+//        printColor();
+//        compareNumbers();
+//        System.out.println(isSumInRange(10, 0));
+//        printNumberSign(10);
+//        System.out.println(isNegative(-10));
+//        printStringNTimes("Хлеб", 10);
+//        System.out.println(isLeapYear(2023));
+//        invertArray();
+//        fillArrayTo100();
+//        doubleIfLessThan6();
+//        fillDiagonals();
+/*        int[] result = createArray(9, 9);
+        for (int value : result) {
+            System.out.print(value + " ");
+        } */
+    }
+
+
+// 1. Создайте метод printThreeWords(), который при вызове должен отпечатать в столбец три слова: Orange, Banana, Apple
+
+    public static void printThreeWords() {
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+
+// 2. Создайте метод checkSumSign(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите.
+// Далее метод должен просуммировать эти переменные, и если их сумма больше или равна 0,то вывести в консоль сообщение “Сумма положительная”, в противном случае - “Сумма отрицательная”;
+
+    public static void checkSumSign() {
+        int a = 2;
+        int b = -2;
+        int sum = a + b;
+        if (sum >= 0) {
+            System.out.println("Сумма положительная");
+        } else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+// 3. Создайте метод printColor() в теле которого задайте int переменную value и инициализируйте ее любым значением.
+// Если value меньше 0 (0 включительно), то в консоль метод должен вывести сообщение “Красный”,
+// если лежит в пределах от 0 (0 исключительно) до 100 (100 включительно), то “Желтый”,
+// если больше 100 (100 исключительно) - “Зеленый”;
+
+    public static void printColor() {
+        int value = -2;
+        if (value <= 0) {
+            System.out.println("Красный");
+        } else if (value <= 100) {
+            System.out.println("Желтый");
+        } else
+            System.out.println("Зеленый");
+    }
+
+
+// 4. Создайте метод compareNumbers(), в теле которого объявите две int переменные a и b, и инициализируйте их любыми значениями, которыми захотите.
+// Если a больше или равно b, то необходимо вывести в консоль сообщение “a >= b”, в противном случае “a < b”;
+
+    public static void compareNumbers() {
+        int a = 2;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a >= b");
+        } else {
+            System.out.println("a < b");
+        }
+    }
+
+// 5. Напишите метод, принимающий на вход два целых числа и проверяющий, что их сумма лежит в пределах от 10 до 20 (включительно), если да – вернуть true, в противном случае – false.
+
+    public static boolean isSumInRange(int a, int b) {
+        int sum = a + b;
+        return sum >= 10 && sum <= 20;
+    }
+
+
+// 6. Напишите метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль, положительное ли число передали или отрицательное.
+// Замечание: ноль считаем положительным числом.
+
+    public static void printNumberSign(int a) {
+        if (a >= 0) {
+            System.out.println(a + " является положительным числом");
+        } else {
+            System.out.println(a + " является отрицательным числом");
+        }
+    }
+
+// 7. Напишите метод, которому в качестве параметра передается целое число. Метод должен вернуть true, если число отрицательное, и вернуть false если положительное.
+// Замечание: ноль считаем положительным числом.
+
+    public static boolean isNegative(int a) {
+        return a < 0;
+    }
+
+// 8. Напишите метод, которому в качестве аргументов передается строка и число, метод должен отпечатать в консоль указанную строку, указанное количество раз;
+
+    public static void printStringNTimes(String a, int b) {
+        for (int i = 0; i < b; i++) {
+            System.out.println(a);
+        }
+    }
+
+// 9. Напишите метод, который определяет, является ли год високосным, и возвращает boolean (високосный - true, не високосный - false).
+// Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный.
+
+    public static boolean isLeapYear(int a) {
+        return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
+    }
+
+// 10. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
+// С помощью цикла и условия заменить 0 на 1, 1 на 0;
+
+    public static void invertArray() {
+        int[] a = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == 0) {
+                a[i] = 1;
+            } else
+                a[i] = 0;
+            System.out.print(a[i] + " ");
+        }
+    }
+
+// 11. Задать пустой целочисленный массив длиной 100.
+// С помощью цикла заполнить его значениями 1 2 3 4 5 6 7 8 ... 100;
+
+    public static void fillArrayTo100() {
+        int[] a = new int[100];
+        for (int i = 0; i < 100; i++) {
+            a[i] = i + 1;
+            System.out.print(a[i] + " ");
+        }
+    }
+
+// 12. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2;
+
+    public static void doubleIfLessThan6() {
+        int[] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < 6) {
+                a[i] = a[i] * 2;
+            }
+            System.out.print(a[i] + " ");
+        }
+    }
+
+// 13. Создать квадратный двумерный целочисленный массив (количество строк и столбцов одинаковое),
+// и с помощью цикла(-ов) заполнить его диагональные элементы единицами (можно только одну из диагоналей, если обе сложно).
+// Определить элементы одной из диагоналей можно по следующему принципу: индексы таких элементов равны, то есть [0][0], [1][1], [2][2], ..., [n][n];
+
+    public static void fillDiagonals() {
+            int a = 9;
+            int[][] array = new int[a][a];
+            for (int i = 0; i < a; i++) {
+                array[i][i] = 1;
+                array[i][a - 1 - i] = 1;
+            }
+            for (int i = 0; i < a; i++) {
+                for (int j = 0; j < a; j++) {
+                    System.out.print(array[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+
+// 14. Написать метод, принимающий на вход два аргумента: len и initialValue,
+// и возвращающий одномерный массив типа int длиной len, каждая ячейка которого равна initialValue.
+
+    public static int[] createArray(int len, int initialValue) {
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            array[i] = initialValue;
+        }
+        return array;
+    }
+}
